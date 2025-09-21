ae2bb65abb14e96a4ebca04155c0f7078118d4e6
Nitya N Hegde <1js20is064@gmail.com>
2025-09-21 04:46:53 +0000

    Now grey background is there

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -13,7 +13,6 @@ export function Hero() {
         className="object-cover"
         priority
       />
-      <div className="absolute inset-0 bg-gradient-to-t from-background via-background/80 to-transparent" />
       <div className="absolute inset-0 bg-black/60" />
       <div className="relative z-10 container px-4 md:px-6 flex flex-col items-center">
         <div className="max-w-3xl space-y-6 text-white">
